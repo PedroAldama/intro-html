523eb68f0fd76ac7c6aec8f694ac81b7464700f0
PedroAldama <pedro.aldama@uan.edu.mx>
2021-08-11 11:39:55 -0600

    Se agraga el archivo js.js que contiene el codigo para que funcione el cambio de imagen, se corrige src en lugar de scr

---
+++ js.js
@@ -0,0 +1,27 @@
+$(function(){
+
+     $('#weiss').click(function(){
+
+
+         $('.masterhead').css({"background-image" : "url('weiss.png')"});
+    });
+
+     $('#ruby').click(function(){
+
+
+         $('.masterhead').css({"background-image" : "url('ruby.jpg')"});
+    });
+     $('#blake').click(function(){
+
+
+         $('.masterhead').css({"background-image" : "url('blake.jpg')"});
+    });
+     $('#yang').click(function(){
+
+
+         $('.masterhead').css({"background-image" : "url('yang.png')"});
+    });
+
+
+
+ });
